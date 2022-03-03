c4555a5b687297ea6d2e446bac57fde2b315ef2f
TarikSogukpinar <hanluee@yandex.com.tr>
2022-03-04 00:33:35 +0300

    New Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,7 @@ function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Open Source FrameWork">
+      description="Roleplay Sunucuları İçin Gelişmiş Roleplay Paketi">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
